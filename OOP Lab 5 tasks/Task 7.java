11b3d5fba80486dc4d9e137011e176507e16d53a
18SW04 <49360457+18SW04@users.noreply.github.com>
2019-05-14 02:59:26 +0500

    Add files via upload

---
+++ OOP Lab 5 tasks/Task 7.java
@@ -0,0 +1,16 @@
+//To write a program that reads two strings
+class Task7{
+	public static void main(String[]args){
+		String s1="Misbah,19,Software;Aeman,18,Software";
+		String str[]=s1.split(";");
+		String str1[]=str[0].split(",");
+		String str2[]=str[1].split(",");
+		System.out.println("Student 1");
+		System.out.println("Name:"+str1[0]);
+		System.out.println("Age:"+str1[1]);
+		System.out.println("Department:"+str1[2]);
+		System.out.println("Student 2");
+		System.out.println("Name:"+str2[0]);
+		System.out.println("Age:"+str2[1]);
+		System.out.println("Department:"+str2[2]);
+		}
